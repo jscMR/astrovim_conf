4b81de5316c5a1ee8ef00b079133940e445808d9
Jorge Serrano <jorge.serrano@rationaltouch.com>
2023-10-31 15:38:51 +0100

    neotree

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -2,6 +2,6 @@ return {{
     "nvim-neo-tree/neo-tree.nvim",
     opts = function(_, opts)
         -- customize sidebar width
-        opts.window.width = 30
+        opts.window.width = 80
     end
 }}
